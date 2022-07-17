f995cc1cb9d82f04ad0d6fb099982fd666bd138c
knshnknd <konoshonkin.d@gmail.com>
2022-07-18 02:21:57 +0400

    Added Russian Chronicle service and new artist to the Wikimedia Commons service

--- src/main/java/ru/knshnknd/chatovyonok/bot/BotKeysConfig.java
+++ src/main/java/ru/knshnknd/chatovyonok/bot/BotKeysConfig.java
@@ -1,10 +1,10 @@
 package ru.knshnknd.chatovyonok.bot;
 
 public class BotKeysConfig {
-    public static final String BOT_TOKEN = "";
+    public static final String BOT_TOKEN = "5300117570:AAGq5KCUQy2pWFMNqMBt7o3O-wHrP5TH61g";
     public static final String BOT_NAME = "@ChatovyonokBot";
 
-    public static final String WEATHER_API_KEY = "";
+    public static final String WEATHER_API_KEY = "dc46a6be427d7a07b3eeffce57e2ce78";
 
-    public static final String YOUTUBE_API_KEY = "";
+    public static final String YOUTUBE_API_KEY = "AIzaSyAM5JwMzacpmhFAGnKc5DQQybpYYX8bXbw";
 }
